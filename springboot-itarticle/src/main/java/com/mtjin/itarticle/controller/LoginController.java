6fb9ec3120b9485dfd18e7c63022203e9b140211
jackjackE <tmddjs210@naver.com>
2020-04-15 23:18:05 +0900

    revise LoginController and register.html, /do_register get ModelAttribute Success

--- springboot-itarticle/src/main/java/com/mtjin/itarticle/controller/LoginController.java
+++ springboot-itarticle/src/main/java/com/mtjin/itarticle/controller/LoginController.java
@@ -1,19 +1,37 @@
 package com.mtjin.itarticle.controller;
 
+import com.mtjin.itarticle.UserDto;
+import com.mtjin.itarticle.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @AllArgsConstructor
 public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView goLogin() {
 
         ModelAndView mv = new ModelAndView();
         mv.setViewName("views/login/login");
+        return mv;
+    }
+
+    @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+    public ModelAndView doRegister(@ModelAttribute(name = "userDto")  UserDto userDto, HttpServletRequest request) {
+        System.out.println(userDto.toString());
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("views/login/register");
         return mv;
     }
 
